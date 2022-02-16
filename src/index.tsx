c927caeb199cd9fc7cf44c26ccb02c8354e58f73
B4D-1D34 <rapkor4@gmail.com>
2022-02-17 00:26:56 +0500

    webpack version of app, without cra

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "./favicon.ico";
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ webpack.config.ts
@@ -0,0 +1,88 @@
+const path = require("path");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+
+import { Configuration } from "webpack";
+
+import "webpack-dev-server";
+
+type Mode = "development" | "production" | "none";
+
+let mode: Mode = "development";
+
+const plugins = [
+  new MiniCssExtractPlugin(),
+  new HtmlWebpackPlugin({
+    template: "./src/index.html",
+  }),
+  new BundleAnalyzerPlugin({
+    analyzerMode: process.env.STATS || "disabled",
+  }),
+];
+
+if (process.env.NODE_ENV === "production") mode = "production";
+else plugins.push(new ReactRefreshWebpackPlugin());
+
+const config: Configuration = {
+  mode: mode,
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "images/[name][ext]",
+    clean: true,
+  },
+
+  optimization: {
+    splitChunks: { chunks: "all" },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg|ico)$/i,
+        type: "asset/resource",
+        // TO CHANGE SIZE THRESHOLD
+        // parser: {
+        //   dataUrlCondition: {
+        //     maxSize: 30 * 1024,
+        //   },
+        // },
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: "ts-loader",
+        options: {
+          transpileOnly: true,
+        },
+      },
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+
+  plugins: plugins,
+
+  resolve: {
+    extensions: [".js", ".tsx", ".ts"],
+  },
+
+  devtool: "source-map",
+
+  devServer: {
+    static: { directory: path.join(__dirname, "dist") },
+    compress: true,
+  },
+};
+
+export default config;
