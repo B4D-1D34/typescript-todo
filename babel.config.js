d5cdf35c5d659b314429a875fb203ef5b3ee994c
B4D-1D34 <rapkor4@gmail.com>
2022-02-17 00:44:17 +0500

    Revert "webpack version of app, without cra"
    This reverts commit c927caeb199cd9fc7cf44c26ccb02c8354e58f73.
    
    reverted to cra version

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-const plugins = [];
-if (process.env.NODE_ENV !== "production") plugins.push("react-refresh/babel");
-module.exports = {
-  presets: [
-    ["@babel/preset-env", { debug: true, useBuiltIns: "usage", corejs: "3" }],
-    ["@babel/preset-react", { runtime: "automatic" }],
-  ],
-  plugins: plugins,
-};
